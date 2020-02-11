0e5d73b007c01bfbfa224c322158b7cf8e701cea
QuentinHsu <xuquentinyang@gmail.com>
2020-02-11 22:01:21 +0800

    chore: 清除浏览器默认样式，添加 Element UI、commitlint插件
    不兼容变更: 无

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'      // 引入 VueRouter
+import 'normalize.css/normalize.css'    // 引入 normalize.css
+import router from '@/router.js'        // 引入 router.js
+
 import App from './App.vue'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)                      // 加载 VueRouter
 
+// 创建和挂载根实例
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
