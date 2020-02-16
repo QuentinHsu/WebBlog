5e8ed0026f3dc03557ad300783c8ef8352bb296e
QuentinHsu <xuquentinyang@gmail.com>
2020-02-16 23:52:12 +0800

    feat(index): 引入特定字体

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <el-row>
-      <el-col span="24">
-        <el-avatar size="20" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-      </el-col>
-      <el-col span="24">
-        <el-card class="window" shadow="never">
-          
-        </el-card>
-      </el-col>
+      <el-card class="window" shadow="never">
+        <el-col :span="24" :offset="14">
+          <el-row>
+            <el-col :span="4">
+              首页
+            </el-col>
+            <el-col :span="4">
+              文档
+            </el-col>
+            <el-col :span="4">
+              关于
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-row>
+          <el-col class="logo" :span="4" :offset="2">
+            <img src="@/assets/logo/LOGO-black.png" alt="logo">
+          </el-col>
+        </el-row>
+      </el-card>
     </el-row>
 
   </div>
 </template>
-
+// TODO: 字体响应式
 <script>
 export default {
 
@@ -23,6 +35,11 @@ export default {
 <style lang="scss">
 .window {
   margin: 0 auto;
-  // width: 80%;
+  width: 80%;
+  font-size: 3vmax;
+  color: black;
+  .logo img {
+    width: 100%;
+  }
 }
 </style>
---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import index from '@/components/index.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: index,
+            component: index
+        },
+        // 使用 Lazy loading 懒加载
+        // {
+        //     path:
+        // }
+    ]
+})
