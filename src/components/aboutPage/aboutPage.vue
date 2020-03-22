79515d6449d855d18631a87255822993beb33f30
QuentinHsu <xuquentinyang@gmail.com>
2020-03-22 10:19:06 +0800

    add(Global): add documentPage and aboutPage

---
+++ src/components/aboutPage/aboutPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-row class="aboutPage">
+
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.aboutPage {
+    min-height: 78.5vh;
+}
+</style>
---
+++ src/components/documentPage/documentPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-row class="documentPage">
+        <h2></h2>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.documentPage {
+    min-height:78.5vh;
+}
+</style>
