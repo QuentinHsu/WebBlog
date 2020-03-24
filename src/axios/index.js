58d50093f268f1a8d319dfb36129554cca54241e
QuentinHsu <xuquentinyang@gmail.com>
2020-03-24 15:07:44 +0800

    add(Global): add axios plugin and basic deploy

---
+++ src/axios/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+axios.interceptors.request.use(config => {
+
+    return config
+},err =>{
+    return Promise.reject(err)
+})
+
+export default axios
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import './styles/normalize.css'   // 清除浏览器默认样式
+import axios from './axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios      // 全局注册使用 axios，使用方法为：$this.axios
 
 new Vue({
   router,
