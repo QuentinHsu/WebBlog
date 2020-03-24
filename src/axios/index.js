68cc14ced73fe3d22518de1b64effc03f483bb89
QuentinHsu <xuquentinyang@gmail.com>
2020-03-24 19:18:37 +0800

    deploy(Global): deploy global HTTP Get request parameters

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
 axios.interceptors.request.use(config => {
+    const apiAccessKey = '123456'
+    if (config.method == 'get') {
+        config.params = {
+            api_access_key: apiAccessKey,
+            ...config.params
+        }
+    }
 
     return config
 },err =>{
