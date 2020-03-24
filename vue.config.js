353de76dfaf218449dcc1b4bca4bb4b2fa05347a
QuentinHsu <xuquentinyang@gmail.com>
2020-03-24 19:05:05 +0800

    deploy(Global): solve the cross-domain problem of development environment

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,27 @@
+// const path = require('path')
+// const debug = 
+
 module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `@import "~@/styles/variableList.scss";`
-      },
-    }
-  }
+	css: {
+		loaderOptions: {
+			scss: {
+				prependData: `@import "~@/styles/variableList.scss";`
+			},
+		}
+	},
+	devServer: {
+        open: true,                                 // 是否自动打开浏览器并进入前端默认页面
+        host: '127.0.0.1',                          // 指定 hostName
+        port: 8081,                                 // 指定端口号
+		proxy: {
+            '/apis': {
+                target: 'http://localhost:8090/',   // api 接口域名
+                secure: false,                       // 若是 https 接口，需要配置这个参数
+                changeOrigin: true,                 // 是否跨域
+                pathRewrite: {
+                    '^/apis': ''
+                }
+            }
+        }
+	}
 }
